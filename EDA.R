bf5aacf0da90a45ccea86defae307de448df9634
Nguyen Doan <nguyendoanbb@gmail.com>
2019-02-01 01:08:56 -0500

    First commit

---
+++ EDA.R
@@ -0,0 +1,76 @@
+#############Exploratory data analysis
+data_bad <- data[which(data$class == 'bad'),]
+data_good <- data[which(data$class == 'good'),]
+
+####categorical features
+library(ggplot2)
+ggplot(data, aes(x=class, fill = housing)) +
+  geom_bar(position = 'stack') +
+  theme(legend.position = 'right') +
+  theme_bw() #majority of people who own a house has good credit,
+#not many people who reside for free have credit card
+ggplot(data, aes(x=class, fill = checking_status)) +
+  geom_bar(position = 'stack') +
+  theme(legend.position = 'right') +
+  theme_bw() #no checking appears to associate with good credit
+ggplot(data, aes(x=class, fill = credit_history))+
+  geom_bar(position = 'stack')+
+  theme(legend.position = 'right') +
+  theme_bw() #all existing paid appears to associate with good credit
+#however, critical account and other existing credit also appears to associate with 
+#good credit instead of bad credit
+ggplot(data, aes(x=class, fill = purpose))+
+  geom_bar(position = 'stack')+
+  theme(legend.position = 'right') +
+  theme_bw() #majority with good credit use card for radio/tv, furniture/equipment, and new car
+ggplot(data, aes(x=class, fill=savings_status))+
+  geom_bar(position = 'stack')+
+  theme(legend.position = 'right') +
+  theme_bw() #saving <100 have more good credit. Probably b/c they depend on credit a lot. 
+#It is easy to notice that people with saving < 100 use credit card the most. 
+ggplot(data, aes(x=class, fill=employment))+
+  geom_bar(position = 'stack')+
+  theme(legend.position = 'right') +
+  theme_bw() #it is unclear to infer anything from this, not a lot of unemployed use credit card (can't pay)
+#or 4<=X<7 use credit card much
+ggplot(data, aes(x=class, fill=personal_status))+
+  geom_bar(position = 'stack')+
+  theme(legend.position = 'right') +
+  theme_bw() #mostly male single and female div/sep/mar use credit card 
+ggplot(data, aes(x=class, fill=other_parties))+
+  geom_bar(position = 'stack')+
+  theme(legend.position = 'right') +
+  theme_bw() #not much to infer from this
+ggplot(data, aes(x=class, fill =property_magnitude))+
+  geom_bar(position = 'stack')+
+  theme(legend.position = 'right') +
+  theme_bw() #people with car and real estate maintain good credit
+ggplot(data, aes(x=class, fill=other_payment_plans))+
+  geom_bar(position = 'stack')+
+  theme(legend.position = 'right') +
+  theme_bw() #not much to infer
+ggplot(data, aes(x=class, fill=job))+
+  geom_bar(position = 'stack')+
+  theme(legend.position = 'right') +
+  theme_bw() #skilled employees maintain good credit
+ggplot(data, aes(x=class, fill=own_telephone))+
+  geom_bar(position = 'stack')+
+  theme(legend.position = 'right') +
+  theme_bw() #not much to infer
+ggplot(data, aes(x=class, fill =foreign_worker))+
+  geom_bar(position = 'stack')+
+  theme(legend.position = 'right') +
+  theme_bw() #a lot of foreing workers use credit card
+
+###continuous features
+ggplot(data, aes(x=class, y=duration, fill=class))+
+  geom_boxplot() +
+  theme_bw()
+ggplot(data, aes(x=credit_amount, fill=class)) + 
+  geom_density(alpha=0.5) + 
+  geom_vline(xintercept = c(mean(data_good$credit_amount), mean(data_bad$credit_amount)), 
+             colour = c('light blue','red'), linetype = 'dashed') #class good has lower credit_amount vs class bad
+#in addition, large portion of people with good credit use less money than people with bad credit do
+ggplot(data, aes(x=class, y=age, fill=class))+
+  geom_boxplot()+
+  theme_bw() #average age with good credit is higher than bad credit 
--- ImportData.R
+++ ImportData.R
@@ -1,4 +1,4 @@
 #Import Data
 rm(list=ls()) #clear current R environment
-setwd("~/Documents/Github/GermanCredit/GermanCredit") #modify to folder contains dataset
-data <- read.csv('dataset_31_credit-g.csv')
+setwd("~/Downloads/") #modify to folder contains dataset
+data <- read.csv('Credit_Dataset.csv')
