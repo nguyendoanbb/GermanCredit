bcfce1119507f48e5e45bc877148259d4480b7fd
Nguyen Doan <nguyendoanbb@gmail.com>
2019-02-02 00:22:21 -0500

    Edited

--- Data_Cleansing.R
+++ Data_Cleansing.R
@@ -10,4 +10,10 @@ for (i in c(1,3:4,6:7,9:10,12,14:15,17,19:20)){
 data$personal_status <- ifelse(data$personal_status == 'female div/dep/mar', 'female div/sep/mar', data$personal_status)
 data <- data %>% mutate_if(is.character, as.factor) #converge character to factor
 str(data) #checking if convergence works
+
+#Divide data into train and test
+set.seed(123)
+train <- sample(1:nrow(data), nrow(data)*2/3)
+data.train <- data[train,]
+data.test <- data[-train,]
 ########################################################################
--- RandomForest.R
+++ RandomForest.R
@@ -1,16 +1,41 @@
 ########################################################################
 #Random forest
-rf.model <- randomForest(class~., data=data.train, importance = TRUE, ntree=300, mtry=5,
-                         nodesize = 40)
+
+#optimize number of variables randomly sampled as candidates at each split (mtry parameter)
+set.seed(1234)
+rf.tune <- tuneRF(x=data.train[,-length(data.train)], y=data.train[,'class'],
+                  stepFactor = 2, trace=TRUE, plot=TRUE, doBest = TRUE)
+
+
+
+rf.model <- randomForest(class~., data=data.train, importance = TRUE,
+                         mtry = rf.tune$mtry)
 plot(rf.model)
+legend('right', colnames(rf.model$err.rate), col=1:3, fill=1:3)
+
 rf.predict <- predict(rf.model, newdata = data.test)
 table(rf.predict, data.test[,'class'], dnn = c('Truth','Predict'))
-1-mean(rf.predict!=data.test$class)
-varImpPlot(rf.model)
+accuracy <- round((1-mean(rf.predict!=data.test$class))*100,2)
+cat('The accuracy by using Random Forest model is: ', accuracy, '%',
+    sep = '')
 
-#searching for optimal number of variables randomly sampled as candidates at each split
-set.seed(1234)
-tuneRF(x=data.train[,-length(data.train)], y=data.train[,'class'],
-       mtry=10, ntree=500,
-       stepFactor = 2, trace=TRUE, plot=TRUE)
+
+
+importance <- importance(rf.model)
+
+varImportance <- data.frame(Variables = row.names(importance), 
+                            Importance = round(importance[ ,'MeanDecreaseGini'],2))
+
+rankImportance <- varImportance %>%
+  mutate(Rank = paste0('#',dense_rank(desc(Importance))))
+
+#ranking importance of features
+ggplot(rankImportance, aes(x = reorder(Variables, Importance) , 
+                           y = Importance, fill = Importance)) +
+  geom_bar(stat='identity') + 
+  geom_text(aes(x = Variables, y = 0.5, label = Rank),
+            hjust=0, vjust=0.55,size = 4, colour = 'red') +
+  labs(x = 'Variables') +
+  coord_flip() +
+  theme_bw()
 ########################################################################
